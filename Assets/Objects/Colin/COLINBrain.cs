8714b1c0da150f60c2b3451c21fcd0358f236d5e
koopthekoopa <eddieatwoodbourne@gmail.com>
2024-10-16 22:40:41 +0100

    Rewrite (last commit is backup)

---
+++ Assets/Objects/Colin/COLINBrain.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COLINBrain : MonoBehaviour
+{
+    public GameObject animObj;
+    
+    void Start()
+    {
+        Debug.Assert(animObj != null);
+    }
+
+    void Update()
+    {
+        
+    }
+}
